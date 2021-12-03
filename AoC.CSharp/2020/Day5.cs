8feea25434dd940c2105d76f8ff605c757b45f20
stew <contact@stew.dev>
2021-12-03 23:34:37 +0000

    2020 Day 5 solution

---
+++ AoC.CSharp/2020/Day5.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC.CSharp._2020
+{
+    public class Day5 : IChallenge
+    {
+        public readonly struct Range
+        {
+            public int From { get; }
+            public int To { get; }
+
+            public Range(int from, int to)
+            {
+                From = from;
+                To = to;
+            }
+
+            public Range TakeLowerHalf()
+            {
+                return new Range(From, From + (To - From) / 2);
+            }
+
+            public Range TakeUpperHalf()
+            {
+                return new Range(From + (To+1 - From) / 2 , To);
+            }
+        }
+
+        public string SolvePart1(string input)
+            => Helper.FilterEmptyOrNull(Helper.SplitByNewLine(input))
+                .Select(s => CalculateSeatId(s))
+                .Max().ToString();
+
+        public int CalculateSeatId(string input)
+        {
+            var range = new Range(0, 127);
+            int i = 0;
+            while (range.From != range.To)
+            {
+                switch (input[i++])
+                {
+                    case 'F':
+                        range = range.TakeLowerHalf();
+                        break;
+                    case 'B':
+                        range = range.TakeUpperHalf();
+                        break;
+                }
+            }
+
+            int row = range.From;
+
+            range = new Range(0, 7);
+            while (range.From != range.To)
+            {
+                switch (input[i++])
+                {
+                    case 'R':
+                        range = range.TakeUpperHalf();
+                        break;
+                    case 'L':
+                        range = range.TakeLowerHalf();
+                        break;
+                }
+            }
+
+            int col = range.From;
+
+            return row * 8 + col;
+        }
+
+        public string SolvePart2(string input)
+        {
+            var seats = Helper.FilterEmptyOrNull(Helper.SplitByNewLine(input)).Select(s => CalculateSeatId(s))
+                .ToHashSet();
+            
+            var ints = 
+                seats
+                .Skip(1)
+                .SkipLast(1)
+                .ToList();
+
+            return Part2Algo(ints).First(i => !seats.Contains(i)).ToString();
+        }
+        
+        private List<int> Part2Algo(List<int> ints)
+        {
+            var rslt = new List<int>();
+            foreach (int i in ints)
+            {
+                foreach (int b in ints)
+                {
+                    if (Math.Abs(i - b) == 2)
+                    {
+                        if (i > b)
+                            rslt.Add(b + 1);
+                        else
+                            rslt.Add(i + 1);
+                    }
+                }
+            }
+
+            return rslt;
+        }
+
+    }
+}
---
+++ test/Aoc.CSharp.Tests/2020/Day5Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using AoC.CSharp._2020;
+using Xunit;
+
+namespace Aoc.CSharp.Tests._2020
+{
+    public class Day5Tests
+    {
+        [Theory]
+        [InlineData(0, 127, 0, 63)]
+        [InlineData(32, 63, 32, 47)]
+        public void RangeTakeLowerHalf(int beginFrom, int beginTo, int expectFrom, int expectTo)
+        {
+            var range = new Day5.Range(beginFrom, beginTo);
+
+            var halvedRange = range.TakeLowerHalf();
+            
+            Assert.Equal(expectFrom, halvedRange.From);
+            Assert.Equal(expectTo, halvedRange.To);
+        }
+
+        [Theory]
+        [InlineData(0, 127, 64, 127)]
+        [InlineData(0, 63, 32, 63)]
+        [InlineData(32, 47, 40, 47)]
+        public void RangeTakeUpperHalf(int beginFrom, int beginTo, int expectFrom, int expectTo)
+        {
+            var range = new Day5.Range(beginFrom, beginTo);
+
+            var halvedRange = range.TakeUpperHalf();
+
+            Assert.Equal(expectFrom, halvedRange.From);
+            Assert.Equal(expectTo, halvedRange.To);
+        }
+        
+        [Theory]
+        [InlineData("FBFBBFFRLR", 357)]
+        [InlineData("BFFFBBFRRR", 567)]
+        [InlineData("FFFBBBFRRR", 119)]
+        [InlineData("BBFFBBFRLL", 820)]
+        public void CalculateSeatId(string input, int expected)
+        {
+            var day4 = new Day5();
+
+            Assert.Equal(expected, day4.CalculateSeatId(input));
+        }
+
+        [Fact]
+        public void SolveDay1()
+        {
+            var input = "FBFBBFFRLR\nBFFFBBFRRR\nFFFBBBFRRR\nBBFFBBFRLL";
+            var day = new Day5();
+
+            Assert.Equal("820", day.SolvePart1(input));
+        }
+        
+    }
+}
