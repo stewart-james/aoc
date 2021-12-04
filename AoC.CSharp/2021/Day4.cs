0af2e5b0f72e6e7bd0f4e433c733c30f5f5059cf
stew <contact@stew.dev>
2021-12-04 15:13:01 +0000

    much neater solution to 2021 Day 4

--- AoC.CSharp/2021/Day4.cs
+++ AoC.CSharp/2021/Day4.cs
@@ -1,124 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AoC.CSharp._2021
 {
-    public class BingoBoard
+    public class Day4 : IChallenge
     {
-        private class BoardCell
+        private bool HasWon(int[] board)
         {
-            public int Number { get; set; }
-            public bool WasCalled { get; private set; }
-
-            public BoardCell(int num)
+            for (int r = 0; r < 5; ++r)
             {
-                Number = num;
-            }
-
-            public void CallNumber(int number)
-            {
-                if (number == Number)
-                    WasCalled = true;
-            }
-        }
-
-        private class BoardRow
-        {
-            private BoardCell[] _cells;
-
-            public BoardRow(int[] numbers)
-            {
-                if (numbers is null)
-                    throw new ArgumentNullException(nameof(numbers));
-
-                if (numbers.Length != 5)
-                    throw new ArgumentException($"Expected 5 numbers but received {numbers.Length}");
-
-                _cells = numbers.Select(n => new BoardCell(n)).ToArray();
-            }
-
-            public void CallNumber(int num)
-            {
-                foreach(var cell in _cells)
-                    cell.CallNumber(num);
-            }
-            
-            public bool HasWon() => _cells.All(c => c.WasCalled);
-
-            public bool CellHasWon(int subscript)
-            {
-                if (subscript < 0 || subscript >= _cells.Length)
-                    throw new ArgumentOutOfRangeException(nameof(subscript), subscript, "invalid subscript passed to CellHasWon");
-
-                return _cells[subscript].WasCalled;
-            }
-
-            public int UnmarkedSum() => _cells.Where(c => !c.WasCalled).Select(c => c.Number).Sum();
-        }
-
-        private readonly BoardRow[] _rows;
-
-        public BingoBoard(int[] numbers)
-        {
-            if (numbers is null)
-                throw new ArgumentNullException(nameof(numbers));
-            
-            if (numbers.Length != 25)
-                throw new ArgumentException($"Expected 25 numbers but {numbers.Length} was provided");
-
-            _rows = new BoardRow[5];
-            for (int i = 0; i < 5; ++i)
-                _rows[i] = new BoardRow(numbers.Skip(i * 5).Take(5).ToArray());
-        }
-
-        public void CallNumber(int num)
-        {
-            foreach (var row in _rows)
-                row.CallNumber(num);
-        }
-
-        public bool HasWon()
-        {
-            if (_rows.Any(r => r.HasWon()))
-                return true;
-            
-            for(int i = 0; i < _rows.Length; ++i)
-                if (ColumnWon(i))
+                // if the row has won
+                if (board.Skip(r* 5).Take(5).All(i => i < 0))
                     return true;
+            }
+            
+            // if any col has won
+            for (int c = 0; c < 5; c++)
+            {
+                bool won = true;
+                for (int i = 0; i < 5; ++i)
+                {
+                    if (board[(i * 5) + c] > 0)
+                    {
+                        won = false;
+                        break;
+                    }
+                }
+
+                if(won)
+                    return true;
+            }
 
             return false;
         }
 
-        public int UnmarkedSum()
-            => _rows.Sum(r => r.UnmarkedSum());
-
-        private bool ColumnWon(int col) => _rows.All(r => r.CellHasWon(col));
-    }
-
-    public class Day4 : IChallenge
-    {
-        public string SolvePart1(string input)
+        private void CallNumber(int n, int[] board)
         {
-            var firstLineSubscript = input.IndexOf('\n');
-            var calledNumbers = input.Substring(0, firstLineSubscript).Split(",").Select(int.Parse);
-
-            var boards = Parse(input.Substring(firstLineSubscript));
-
-            foreach (var num in calledNumbers)
-            {
-                foreach (var board in boards)
-                {
-                    board.CallNumber(num);
-
-                    if (board.HasWon())
-                        return (board.UnmarkedSum() * num).ToString();
-                }
-            }
-
-            throw new Exception("Failed to find solution");
+            for(int i = 0; i < board.Length; ++i)
+                if (board[i] == n)
+                    board[i] = -Math.Abs(n);
         }
+
+        private int UnmarkedSun(int[] board)
+        {
+            int sum = 0;
+            for(int i = 0; i < board.Length; ++i)
+                if (board[i] > 0)
+                    sum += board[i];
+
+            return sum;
+        }
+
+        IEnumerable<int> CalculateScores(List<int> calledNumbers, List<int[]> boards) =>
+            calledNumbers.Select(n => boards.Select(b =>
+            {
+                if (!HasWon(b))
+                {
+                    CallNumber(n, b);
+
+                    if (HasWon(b))
+                        return UnmarkedSun(b) * n;
+                }
+
+                return 0;
+            })).SelectMany(scores => scores.Where(n => n > 0));
+
+        IEnumerable<int> CalculateScores(string input)
+        {
+            var calledNumbers =
+                input.Substring(0, input.IndexOf('\n')).Split(",").Select(int.Parse).ToList();
+            
+            List<int[]> boards = Parse(input.Substring(input.IndexOf('\n')));
+
+            return CalculateScores(calledNumbers, boards);
+        }
+
+        public string SolvePart1(string input)
+            => CalculateScores(input).First().ToString();
+
+        public string SolvePart2(string input)
+            => CalculateScores(input).Last().ToString();
         
-        public static BingoBoard[] Parse(string input)
+        private static List<int[]> Parse(string input)
         {
             return input
                 .Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
@@ -127,34 +91,8 @@ namespace AoC.CSharp._2021
                     .Replace("  ", " ")
                     .Trim()
                     .Split(" ")
-                    .Select(int.Parse)).Select(e => new BingoBoard(e.ToArray())).ToArray();
-        }
-
-        public string SolvePart2(string input)
-        {
-            var firstLineSubscript = input.IndexOf('\n');
-            var calledNumbers = input.Substring(0, firstLineSubscript).Split(",").Select(int.Parse);
-
-            var boards = Parse(input.Substring(firstLineSubscript));
-
-            foreach (var num in calledNumbers)
-            {
-                var losingBoards = boards.Where(b => !b.HasWon()).ToList();
-                int wonCount = 0;
-
-                foreach (var board in losingBoards)
-                {
-                    board.CallNumber(num);
-
-                    if (board.HasWon())
-                        wonCount++;
-                    
-                    if(losingBoards.Count - wonCount == 0)
-                        return (board.UnmarkedSum() * num).ToString();
-                }
-            }
-
-            throw new Exception("Could not find part 2 solution");
+                    .Select(int.Parse))
+                .Select(e => e.ToArray()).ToList();
         }
     }
 }
