2f1569f4e3449b6e6feba4ddb15e21235ced1d24
stew <contact@stew.dev>
2021-12-07 13:36:41 +0000

    implemented LINQ solution to day7

--- AoC.CSharp/2021/Day7.cs
+++ AoC.CSharp/2021/Day7.cs
@@ -8,29 +8,17 @@ namespace AoC.CSharp._2021
     public class Day7 : IChallenge
     {
         public string SolvePart1(string input)
-        {
-            var ints = Helper.ParseInts(input.Split(",")).ToList();
-
-            var horizontalPositionByFuelRequired = new Dictionary<int, int>();
-            for (int i = 0; i < ints.Max(); ++i)
-                horizontalPositionByFuelRequired[i] = ints.Sum(n => Math.Abs(i - n));
-
-            var bestPosition = horizontalPositionByFuelRequired.Min(k => k.Value);
-
-            return bestPosition.ToString();
-        }
+            => Solve(Helper.ParseInts(input.Split(",")).ToList(), true);
 
         public string SolvePart2(string input)
-        {
-            var ints = Helper.ParseInts(input.Split(",")).ToList();
+            => Solve(Helper.ParseInts(input.Split(",")).ToList(), false);
 
-            var horizontalPositionByFuelRequired = new Dictionary<int, int>();
-            for (int i = 1; i < ints.Max(); ++i)
-                horizontalPositionByFuelRequired[i] = ints.Sum(n => Math.Abs(i - n) * (Math.Abs(i - n)+1) / 2);
-
-            var bestPosition = horizontalPositionByFuelRequired.Min(k => k.Value);
-
-            return bestPosition.ToString();
-        }
+        private string Solve(List<int> input, bool constantFuel)
+            => Enumerable.Range(1, input.Max())
+                .Select(i =>
+                    (i, input.Sum(n => constantFuel ? Math.Abs(i - n) : Math.Abs(i - n) * (Math.Abs(i - n) + 1) / 2)))
+                .ToDictionary(i => i.Item1)
+                .Min(k => k.Value.Item2)
+                .ToString();
     }
 }
