2d70ce64db20a7bd9a98b0a6d1ea4487e4e7b3df
stew <contact@stew.dev>
2022-05-12 22:53:39 +0100

    LINQ solution to 2015 day2

---
+++ AoC.CSharp/2015/Day2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AoC.CSharp._2015
+{
+    public class Day2 : IChallenge
+    {
+        private IEnumerable<List<int>> ParseInput(string input)
+            => input
+                .TrimEnd('\n')
+                .Split("\n")
+                .Select(s => s.Split("x"))
+                .Select(s => s.Select(int.Parse)
+                    .ToList());
+        
+        public string SolvePart1(string input)
+            => ParseInput(input) 
+                .Select(Sides)
+                .Sum(sides => sides.Min() + sides.Sum(side => side * 2))
+                .ToString();
+        
+        private const int Length = 0;
+        private const int Width = 1;
+        private const int Height = 2;
+
+        private static List<int> Sides(List<int> dimensions) => new()
+        {
+            dimensions[Length] * dimensions[Width],
+            dimensions[Width] * dimensions[Height],
+            dimensions[Height] * dimensions[Length]
+        };
+
+        public string SolvePart2(string input)
+            => ParseInput(input)
+                .Sum(dimenions => CalculateRibbon(dimenions) + CalculateBow(dimenions))
+                .ToString();
+
+        private static int CalculateRibbon(List<int> dimensions)
+            => dimensions
+                .OrderBy(i => i)
+                .Take(2)
+                .Sum(side => side * 2);
+        private static int CalculateBow(List<int> dimensions)
+            => dimensions.Aggregate(1, (acc, side) => acc * side);
+    }
+}
