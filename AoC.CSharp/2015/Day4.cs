4022e620431b631d3257e417d7ff5ddb7c43c4c9
stew <contact@stew.dev>
2022-05-13 19:29:03 +0100

    2015 day 4 linq solution

---
+++ AoC.CSharp/2015/Day4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace AoC.CSharp._2015
+{
+    public class Day4 : IChallenge
+    {
+        public string SolvePart1(string input)
+            => FindMatch(input.TrimEnd('\n'), 5);
+
+        public string SolvePart2(string input)
+            => FindMatch(input.TrimEnd('\n'), 6);
+        
+        private string FindMatch(string input, int count) =>
+            Enumerable.Range(0, Int32.MaxValue)
+                .First(i => IsMatch(Hash(input + i), count))
+                .ToString();
+
+        private bool IsMatch(string result, int count)
+            => result.Take(count).All(c => c == '0');
+        private static string Hash(string content)
+        {
+            // Use input string to calculate MD5 hash
+            using (var md5 = MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(content);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes); // .NET 5 +
+            }
+        }
+    }
+}
---
+++ test/Aoc.CSharp.Tests/2015/Day4Tests.cs
@@ -0,0 +1,14 @@
+﻿using AoC.CSharp._2015;
+using Xunit;
+
+namespace Aoc.CSharp.Tests._2015
+{
+    public class Day4Tests
+    {
+        [Theory]
+        [InlineData("abcdef", "609043")]
+        [InlineData("pqrstuv", "1048970")]
+        public void Part1(string input, string expected) =>
+            Assert.Equal(expected, new Day4().SolvePart1(input));
+    }
+}
