95b201596d4c6f6501a6f4be34390ee8925aab18
stew <contact@stew.dev>
2022-05-13 19:10:47 +0100

    LINQ solution 2015 day 3 part 1 and 2

---
+++ AoC.CSharp/2015/Day3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace AoC.CSharp._2015
+{
+    public class Day3 : IChallenge
+    {
+        public string SolvePart1(string input) 
+            => PlotMoves(input.TrimEnd('\n'))
+                .GroupBy(c => c)
+                .Count()
+                .ToString();
+
+        public string SolvePart2(string input) => 
+            PlotMoves(ExtractPath(input.TrimEnd('\n'), false))
+                .Concat(PlotMoves(ExtractPath(input.TrimEnd('\n'), true)))
+                .Distinct()
+                .Count() 
+                .ToString();
+
+        private IEnumerable<(int, int)> PlotMoves(IEnumerable<char> input) =>
+            input
+                .Aggregate(new List<(int, int)> { (0, 0) }, (acc, move) =>
+                    acc.Append(MakeMove(acc.Last(), move)).ToList());
+        
+        private IEnumerable<char> ExtractPath(string input, bool evilSanta)
+            => Enumerable.Range(0, input.Length)
+                .Where(idx => evilSanta ? (idx + 1) % 2 == 0 : (idx + 1) % 2 != 0) 
+                .Select(idx => input[idx]);
+
+        private (int, int) MakeMove((int, int) current, char direction)
+            => (current.Item1 + MoveX(direction), current.Item2 + MoveY(direction));
+        
+        private int MoveX(char direction) =>
+            direction switch
+            {
+                '>' => 1,
+                '<' => -1,
+                _ => 0
+            };
+
+        private int MoveY(char direction) =>
+            direction switch {
+                '^' => 1,
+                'v' => -1,
+                _ => 0
+            };
+    }
+}
---
+++ test/Aoc.CSharp.Tests/2015/Day3Tests.cs
@@ -0,0 +1,22 @@
+﻿using AoC.CSharp._2015;
+using Xunit;
+
+namespace Aoc.CSharp.Tests._2015
+{
+    public class Day3Tests
+    {
+        [Theory]
+        [InlineData(">", "2")]
+        [InlineData("^>v<", "4")]
+        [InlineData("^v^v^v^v^v", "2")]
+        public void Part1(string input, string expected)
+            => Assert.Equal(expected, new Day3().SolvePart1(input));
+        
+        [Theory]
+        [InlineData("^v", "3")]
+        [InlineData("^>v<", "3")]
+        [InlineData("^v^v^v^v^v", "11")]
+        public void Part2(string input, string expected)
+            => Assert.Equal(expected, new Day3().SolvePart2(input));
+    }
+}
