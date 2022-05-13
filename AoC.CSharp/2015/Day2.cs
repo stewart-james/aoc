d0d69668a1bcf9f1b8e190213ed5f04a2c328f3b
stew <contact@stew.dev>
2022-05-13 17:30:15 +0100

    comments

--- AoC.CSharp/2015/Day2.cs
+++ AoC.CSharp/2015/Day2.cs
@@ -14,6 +14,9 @@ namespace AoC.CSharp._2015
                 .Select(s => s.Select(int.Parse)
                     .ToList());
         
+        // Calculating wrapping paper where the total wrapping paper per present is calculated as
+        // smallest side + total area of present
+        // e.g 2x3x6 | smallest side = 6 (2x3) | total area = 52 ( 2*(2*3) + 2*(3*4) + 2*(2*4) )
         public string SolvePart1(string input)
             => ParseInput(input) 
                 .Select(Sides)
@@ -31,6 +34,15 @@ namespace AoC.CSharp._2015
             dimensions[Height] * dimensions[Length]
         };
 
+        // Ribbon to wrap the present
+        //
+        // Ribbon calculated as: shortest distance around the sides
+        //  2x3x4 = 2+2+3+3 = 10
+        //
+        // Bow calculated as: volume of present
+        //  2x3x4 = 2*3*4 = 24
+        // 
+        // total ribbon required = 10 + 24 = 34
         public string SolvePart2(string input)
             => ParseInput(input)
                 .Sum(dimenions => CalculateRibbon(dimenions) + CalculateBow(dimenions))
