80fe952659cf1deadbded528e814bb241be83961
stew <contact@stew.dev>
2022-05-14 10:04:05 +0100

    2015 day 5 linq + regex fun

---
+++ AoC.CSharp/2015/Day5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC.CSharp._2015
+{
+    public class Day5 : IChallenge
+    {
+        private readonly Func<string, bool> _mustNotContain = s => !Regex.IsMatch(s, "(ab|cd|pq|xy)");
+        private readonly Func<string, bool> _mustContain3Vowels = s => s.Count("aeiou".Contains) >= 3;
+        private readonly Func<string, bool> _mustContainPairOfIdenticalLetters = s => Regex.IsMatch(s, @"([a-z])\1");
+        
+        public string SolvePart1(string input) =>
+            input
+                .TrimEnd('\n')
+                .Split('\n')
+                .Count(s => _mustNotContain(s) && _mustContain3Vowels(s) && _mustContainPairOfIdenticalLetters(s))
+                .ToString();
+
+        private readonly Func<string, bool> _containsPairThatAppearsTwiceWithoutOverlapping = s => Regex.IsMatch(s, @"([a-z][a-z]).*\1");
+        private readonly Func<string, bool> _containsRepeatWithOneCharBetween = s => Regex.IsMatch(s, @"([a-z])[a-z]\1");
+
+        public string SolvePart2(string input)
+            => input
+                .TrimEnd('\n')
+                .Split('\n')
+                .Count(s => _containsRepeatWithOneCharBetween(s) && 
+                            _containsPairThatAppearsTwiceWithoutOverlapping(s))
+                .ToString();
+    }
+}
---
+++ test/Aoc.CSharp.Tests/2015/Day5Tests.cs
@@ -0,0 +1,25 @@
+﻿using AoC.CSharp._2015;
+using Xunit;
+
+namespace Aoc.CSharp.Tests._2015
+{
+    public class Day5Tests
+    {
+        [Theory]
+        [InlineData("ugknbfddgicrmopn", "1")]
+        [InlineData("aaa", "1")]
+        [InlineData("jchzalrnumimnmhp", "0")]
+        [InlineData("haegwjzuvuyypxyu", "0")]
+        [InlineData("dvszwmarrgswjxmb", "0")]
+        public void SolvePart1(string input, string expected)
+            => Assert.Equal(expected, new Day5().SolvePart1(input));
+        
+        [Theory]
+        [InlineData("qjhvhtzxzqqjkmpb", "1")]
+        [InlineData("xxyxx", "1")]
+        [InlineData("uurcxstgmygtbstg", "0")]
+        [InlineData("ieodomkazucvgmuy", "0")]
+        public void SolvePart2(string input, string expected)
+            => Assert.Equal(expected, new Day5().SolvePart2(input));
+    }
+}
